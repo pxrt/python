49b605f96a5b68267417b5f7a22d5426907d64c9
pxrt <parthbhayani@protonmail.com>
2021-07-09 14:28:51 +0530

    Added all Python Scripts to the repo

---
+++ 10. Functions.py
@@ -0,0 +1,13 @@
+def oranges():
+    print("Oranges makes me happy.")
+
+oranges()
+
+def etherium_to_usd(ETH):
+    amount = ETH * 200
+    print (amount)
+
+oranges()
+etherium_to_usd(5)
+etherium_to_usd(10)
+etherium_to_usd(50)
