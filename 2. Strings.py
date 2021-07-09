afe62acbdfeb29315b65ab28cce5385077aa0e45
pxrt <parthbhayani@protonmail.com>
2021-07-09 14:11:59 +0530

    Added 2. Strings to the repo

---
+++ 2. Strings.py
@@ -0,0 +1,9 @@
+print("hello world")
+print('hello world')
+print('don\'t worry')
+print("don't worry")
+print('She said, "I don\'t want to eat oranges."')
+
+# \n will try to move the rest of the content to new line.
+
+print(r'C:\Desktop\NorwayPictures')
